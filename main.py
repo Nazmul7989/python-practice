14e7ae736199ad4b14c5633daa48924f1e36437b
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-20 09:07:33 +0600

    syntax, comment, output, variable

--- main.py
+++ main.py
@@ -1,6 +1,98 @@
+"""
 name = "Md Nazmul Hasan"
 
 print(name)
 
 import sys
 print(sys.version_info)
+"""
+
+# if 5 > 2:
+#     print("5 is greater than 2")
+#
+# if 5 > 2:
+#  print("Five is greater than two!")
+# if 5 > 2:
+#         print("Five is greater than two!")
+#         print("Five is greater than two!")
+#
+# print("Hello"); print("How are you?"); print("Bye bye!")
+#
+# name = "Md Nazmul Hasan"; age = 30
+#
+# print(name, age)
+#
+# print("Hello World!", end=" ")
+# print("I will print on the same line.")
+
+# print("I am", 35, "years old.")
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+# print(x)
+# print(y)
+# print(z)
+
+# a = True
+# b = False
+#
+# print(a)
+# print(b)
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+#
+# x = 5
+# y = 10
+# print(x + y)
+
+# x = 5
+# y = "John"
+# print(x , y)
+
+
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
