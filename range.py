f522dd6cd54b46f726160f659e507cbcf191088b
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-21 18:22:54 +0600

    range

---
+++ range.py
@@ -0,0 +1,39 @@
+# x = range(10)
+# print(x)
+#
+# for i in x:
+#     print(i)
+
+# x = range(1,10)
+# print(x)
+#
+# for i in x:
+#     print(i)
+
+# x = range(1,10,2)
+# print(x)
+#
+# for i in x:
+#     print(i)
+
+# print(list(range(5)))
+# print(list(range(1, 6)))
+# print(list(range(5, 20, 3)))
+
+# r = range(10)
+# print(r[2])
+# print(r[:3])
+# print(r[1:])
+
+
+# r = range(0, 10, 2)
+# print(6 in r)
+# print(7 in r)
+
+# r = range(0, 10, 2)
+# for i in r:
+#     print(i)
+#
+# print(len(r))
+
+
