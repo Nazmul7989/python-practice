e63a78497af5cef040b8a5b2e4f6783638f4ba0a
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-21 18:08:35 +0600

    python boolean

---
+++ boolean.py
@@ -0,0 +1,42 @@
+# print(10 > 9) #True
+# print(10 == 9) #False
+# print(10 < 9) #False
+
+# print(bool("Hello"))
+# print(bool(15))
+#
+# x = "Hello"
+# y = 15
+#
+# print(bool(x))
+# print(bool(y))
+
+# print(bool("abc"))
+# print(bool(123))
+# print(bool(["apple", "cherry", "banana"]))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+
+# def myfunction() :
+#   return True
+#
+# print(myfunction())
+
+#
+# def myfunction() :
+#   return True
+#
+# if myfunction():
+#   print("YES!")
+# else:
+#   print("NO!")
+
+x = 200
+print(isinstance(x, int)) #True
+print(isinstance(x, str)) #False
