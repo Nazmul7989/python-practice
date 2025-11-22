1788cef8ab6be8ee3544719274cb1e9f387cc76e
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-22 10:45:50 +0600

    conditional statement

---
+++ ifelse.py
@@ -0,0 +1,54 @@
+# score = 75
+#
+# if score >= 90:
+#   print("Grade: A")
+# elif score >= 80:
+#   print("Grade: B")
+# elif score >= 70:
+#   print("Grade: C")
+# elif score >= 60:
+#   print("Grade: D")
+# else:
+#   print("Grade: F")
+
+# a = 20
+# b = 10
+
+# print('A') if a > b else print('B')
+
+# Multiple condition in one line
+# print("A") if a > b else print("=") if a == b else print("B")
+
+# a = 200
+# b = 33
+# c = 500
+# if a > b and c > a:
+#   print("Both conditions are True")
+
+# age = 25
+# is_student = False
+# has_discount_code = True
+#
+# if (age < 18 or age > 65) and not is_student or has_discount_code:
+#   print("Discount applies!")
+
+
+# age = 25
+# has_license = False
+#
+# if age >= 18:
+#   if has_license:
+#     print("You can drive")
+#   else:
+#     print("You need a license")
+# else:
+#   print("You are too young to drive")
+
+# value = 50
+#
+# if value < 0:
+#   print("Negative value")
+# elif value == 0:
+#   pass # Zero case - no action needed
+# else:
+#   print("Positive value")
