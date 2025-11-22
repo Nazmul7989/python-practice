94478d89ff04448708873bb1f726b52774cde101
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-22 12:17:51 +0600

    math module

---
+++ math-module.py
@@ -0,0 +1,37 @@
+# x = min(5, 10, 25)
+# y = max(5, 10, 25)
+
+# print(x)
+# print(y)
+
+# x = abs(-7.25)
+# print(x)
+
+# x = pow(4, 3)
+# print(x)
+
+import math
+
+# print(math.pi)
+# print(math.e)
+
+# print(math.sqrt(16))
+# print(math.ceil(16.1))
+# print(math.floor(16.9))
+# print(math.factorial(5))
+# print(math.gcd(16, 12))
+# print(math.lcm(16, 12))
+# print(math.pow(4, 2))
+
+import random
+
+# print(random.randint(1, 10))
+# print(random.random())
+
+# print(random.choice([1, 2, 3, 4, 5]))
+# print(random.choice(["apple", "banana", "cherry"]))
+
+# print(random.shuffle([1, 2, 3, 4, 5]))
+print(random.sample([1, 2, 3, 4, 5], 3))
+
+
