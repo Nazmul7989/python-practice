f78f240e38bc2328153ef47fbe93e35f456294da
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-22 11:02:20 +0600

    loop practice

---
+++ loop.py
@@ -0,0 +1,45 @@
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("Goodbye")
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+#   if i == 3:
+#     break
+#
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#
+#     print(i)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+# else:
+#   print("Goodbye")
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#
+#   if x == "banana":
+#     continue
+#
+#   print(x)
+
+# for x in range(6):
+#   print(x)
