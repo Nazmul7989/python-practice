c8458294c842f3636f7e53d59c139cc75afceddf
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-22 08:20:28 +0600

    tuple practice

---
+++ tuple.py
@@ -0,0 +1,77 @@
+mytuple = ("apple", "banana", "cherry")
+# mytuple = tuple(("apple", "banana", "cherry"))
+# print(mytuple)
+# print(type(mytuple))
+# print(len(mytuple))
+# print(mytuple[1])
+# print(mytuple[-1])
+# print(mytuple[-2])
+# print(mytuple[-3])
+# print(mytuple[:2])
+# print(mytuple[1:])
+# print(mytuple[-3:])
+# print(mytuple[:-1])
+# print(mytuple[-3:-1])
+
+# for x in mytuple:
+#     print(x)
+
+# if "apple" in mytuple:
+#     print("Yes, 'apple' is in the fruits tuple")
+# else:
+#     print("No, 'apple' is not in the fruits tuple")
+
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+#NOT a tuple
+# thistuple = ("apple")
+# print(type(thistuple))
+
+# mylist = list(mytuple)
+# mylist[1] = "orange"
+# mytuple = tuple(mylist)
+# print(mytuple)
+
+# del mytuple
+# print(mytuple)
+
+# (green, yellow, red) = mytuple
+#
+# print(green)
+# print(yellow)
+# print(red)
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+#
+# (green, yellow, *red) = fruits
+#
+# print(green)
+# print(yellow)
+# print(red)
+
+# for x in mytuple:
+#     print(x)
+
+# for x in range(len(mytuple)):
+#     print(x)
+#     print(mytuple[x])
+
+# i = 0
+# while i < len(mytuple):
+#     print(i)
+#     print(mytuple[i])
+#     i = i + 1
+
+
+# tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+#
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+# newTuple = mytuple * 2
+# print(newTuple)
+
+# print(mytuple.index('apple'))
+# print(mytuple.count('apple'))
