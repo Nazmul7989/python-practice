f8da2275dcfced63ed976574bd32c6b6a0070f0c
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2025-11-22 12:34:43 +0600

    date module

---
+++ date-module.py
@@ -0,0 +1,35 @@
+import datetime
+
+# print(datetime.datetime.now())
+# print(datetime.datetime.now().year)
+# print(datetime.datetime.now().month)
+# print(datetime.datetime.now().day)
+# print(datetime.datetime.now().hour)
+# print(datetime.datetime.now().minute)
+# print(datetime.datetime.now().second)
+# print(datetime.datetime.now().microsecond)
+
+# x = datetime.datetime.now()
+#
+# print(x.year)
+# print(x.strftime("%A"))
+
+# x = datetime.datetime(2020, 5, 17, 12,24,30)
+# print(x)
+
+x = datetime.datetime.now()
+# print(x.strftime("%p"))
+print(x.strftime("%d-%m-%Y, %I:%M:%S %p"))
+
+
+
+
+
+# print(datetime.date.today())
+# print(datetime.date.today().year)
+# print(datetime.date.today().month)
+# print(datetime.date.today().day)
+
+
+
+
